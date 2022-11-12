21cbbf59d1b5aca6be9bef21bdfc6ddeba854c2b
Andrey Kotlyar <kotlyar562@gmail.com>
2022-11-12 22:53:46 +0200

    add TopBar component

---
+++ src/components/TopBar/TopBar.styles.ts
@@ -0,0 +1,24 @@
+import {ScaledSheet} from 'react-native-size-matters';
+import {Theme} from '~/theming';
+
+const createStyles = (theme: Theme) => {
+  const styles = ScaledSheet.create({
+    container: {
+      height: '40@vs',
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginBottom: '4@vs',
+      padding: '4@vs',
+      backgroundColor: theme.colors.primary,
+    },
+    title: {
+      fontFamily: 'SpriteGraffiti',
+      fontSize: '16@s',
+      flexWrap: 'wrap',
+      color: theme.colors.onPrimary,
+    },
+  });
+  return styles;
+};
+export default createStyles;
