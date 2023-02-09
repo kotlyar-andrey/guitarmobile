e334702b929bd1eade0f0451edf741935d2e50a0
Andrey Kotlyar <kotlyar562@gmail.com>
2023-02-09 21:31:49 +0200

    add accordion container

---
+++ src/shared/components/AccordionContainer/AccordionContainer.styles.ts
@@ -0,0 +1,37 @@
+import {ScaledSheet} from 'react-native-size-matters';
+import {Theme} from '~/entities/theming';
+
+const createStyles = (theme: Theme) => {
+  const styles = ScaledSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+    },
+    titleContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingHorizontal: '4@s',
+      alignItems: 'center',
+      borderBottomWidth: 1,
+      borderBottomColor: theme.colors.secondary,
+    },
+    toolbarContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    toolbarItem: {
+      marginRight: '4@s',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    title: {
+      textAlign: 'center',
+      color: theme.colors.text,
+      fontSize: '9@s',
+      textDecorationColor: theme.colors.primary,
+    },
+  });
+  return styles;
+};
+export default createStyles;
---
+++ src/shared/components/AccordionContainer/AccordionContainer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useTheme} from '~/features/themeSwitcher';
+import createStyles from './AccordionContainer.styles';
+
+type Props = {
+  title: string;
+  visible: boolean;
+  toggleVisible: () => void;
+  children: React.ReactNode;
+};
+export const AccordionContainer: React.FC<Props> = ({
+  title,
+  visible,
+  toggleVisible,
+  children,
+}) => {
+  const theme = useTheme();
+  const styles = createStyles(theme);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text />
+        {visible && <Text style={styles.title}>{title}</Text>}
+        <View style={styles.toolbarContainer}>
+          <TouchableOpacity style={styles.toolbarItem} onPress={toggleVisible}>
+            {visible ? (
+              <MaterialCommunityIcons
+                name="eye-off-outline"
+                color={theme.colors.secondary}
+                size={moderateScale(14)}
+              />
+            ) : (
+              <>
+                <Text>Показать {title.toLowerCase()}</Text>
+                <MaterialCommunityIcons
+                  name="eye-off-outline"
+                  color={theme.colors.secondary}
+                  size={moderateScale(14)}
+                />
+              </>
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+      {visible && <View>{children}</View>}
+    </View>
+  );
+};
